1919c72d19b8a16787f425d9c31eca5acd8c797a
MR5553 <shauryakumar42040er@gmail.com>
2025-05-24 23:14:50 +0530

    Remove unused filesystem

--- src/middleware/multer.middleware.ts
+++ src/middleware/multer.middleware.ts
@@ -1,11 +1,7 @@
 import multer from "multer";
 import { v6 as uuid } from "uuid";
 import path from "node:path";
-import fs from "node:fs";
 
-
-const temp = path.join(__dirname, "./public/temp")
-if (!fs.existsSync(temp)) fs.mkdirSync(temp, { recursive: true });
 
 const storage = multer.diskStorage({
     destination(_req, _file, cb) {
